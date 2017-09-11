8bd8d51e6debec6a9e03dbc84374aee1c4eab9c9
Aaron Pando Sola <apando@dnestagency.com>
2017-09-11 14:11:56 +0200

    Building MC2P client

--- mc2p/MC2P.php
+++ mc2p/MC2P.php
@@ -32,27 +32,112 @@ class APIClient
         $this->sale = new SaleResource($this->apiRequest);
         $this->currency = new CurrencyResource($this->apiRequest);
         $this->gateway = new GatewayResource($this->apiRequest);
-        $this->payData = new PayDataResource($this->apiRequest);   
-
-        $this->Product = decorator('MyChoice2Pay\Product', $this->product);
-        $this->Plan = decorator('MyChoice2Pay\Plan', $this->plan);
-        $this->Tax = decorator('MyChoice2Pay\Tax', $this->tax);
-        $this->Shipping = decorator('MyChoice2Pay\Shipping', $this->shipping);
-        $this->Coupon = decorator('MyChoice2Pay\Coupon', $this->coupon);
-        $this->Transaction = decorator('MyChoice2Pay\Transaction', $this->transaction);
-        $this->Subscription = decorator('MyChoice2Pay\Subscription', $this->subscription);
-        $this->Sale = decorator('MyChoice2Pay\Sale', $this->sale);
-        $this->Currency = decorator('MyChoice2Pay\Currency', $this->currency);
-        $this->Gateway = decorator('MyChoice2Pay\Gateway', $this->gateway);
-        $this->PayData = decorator('MyChoice2Pay\PayData', $this->payData);
-
-        $this->NotificationData = decorator('MyChoice2Pay\NotificationData', $this);
+        $this->payData = new PayDataResource($this->apiRequest);
     }
-}
 
-function decorator($class, $resource)
-{
-    $payload = $class::get();
-    $obj = new $class($payload, $resource);
-    return $obj;
+    /**
+     * @param string    $class; Object class name
+     * @param object    $resource; Resource instance
+     * @param array     $payload
+     */
+    private function __wrapper ($class, $resource, $payload) 
+    {
+        return new $class ($payload, $resource, $payload);
+    }
+
+    /**
+     * @param array   $payload
+     */
+    public function Product ($payload = array()) 
+    {
+        return $this->__wrapper('MyChoice2Pay\Product', $this->product, $payload);
+    }
+
+    /**
+     * @param array   $payload
+     */
+    public function Plan ($payload = array()) 
+    {
+        return $this->__wrapper('MyChoice2Pay\Plan', $this->plan, $payload);
+    }
+
+    /**
+     * @param array   $payload
+     */
+    public function Tax ($payload = array()) 
+    {
+        return $this->__wrapper('MyChoice2Pay\Tax', $this->tax, $payload);
+    }
+
+    /**
+     * @param array   $payload
+     */
+    public function Shipping ($payload = array()) 
+    {
+        return $this->__wrapper('MyChoice2Pay\Shipping', $this->shipping, $payload);
+    }
+
+    /**
+     * @param array   $payload
+     */
+    public function Coupon ($payload = array()) 
+    {
+        return $this->__wrapper('MyChoice2Pay\Coupon', $this->coupon, $payload);
+    }
+
+    /**
+     * @param array   $payload
+     */
+    public function Transaction ($payload = array()) 
+    {
+        return $this->__wrapper('MyChoice2Pay\Transaction', $this->transaction, $payload);
+    }
+
+    /**
+     * @param array   $payload
+     */
+    public function Subscription ($payload = array()) 
+    {
+        return $this->__wrapper('MyChoice2Pay\Subscription', $this->subscription, $payload);
+    }
+
+    /**
+     * @param array   $payload
+     */
+    public function Sale ($payload = array()) 
+    {
+        return $this->__wrapper('MyChoice2Pay\Sale', $this->sale, $payload);
+    }
+
+    /**
+     * @param array   $payload
+     */
+    public function Currency ($payload = array()) 
+    {
+        return $this->__wrapper('MyChoice2Pay\Currency', $this->currency, $payload);
+    }
+
+    /**
+     * @param array   $payload
+     */
+    public function Gateway ($payload = array()) 
+    {
+        return $this->__wrapper('MyChoice2Pay\Gateway', $this->gateway, $payload);
+    }
+
+    /**
+     * @param array   $payload
+     */
+    public function PayData ($payload = array()) 
+    {
+        return $this->__wrapper('MyChoice2Pay\PayData', $this->payData, $payload);
+    }
+
+    /**
+     * @param array   $payload
+     */
+    public function NotificationData ($payload = array()) 
+    {
+        return $this->__wrapper('MyChoice2Pay\NotificationData', $this, $payload);
+    }
 }
