afaf5d73f0be11bc3441852ce749c4eb3ed26765
Abel Puentes Luberta <apuentes@abalt.org>
2017-09-12 22:40:50 +0200

    Added tests

--- tests/mc2p.php
+++ tests/mc2p.php
@@ -32,23 +32,23 @@ try {
     $iURL = $transaction->getIframeUrl();
 
     var_dump(
-        array('------------TRANSACTION---------------'),
-        array('------------PAY URL---------------', $pURL),
-        array('------------IFRAME URL------------', $iURL)
+        array('--------TRANSACTION TEST---------'),
+        array('------------PAY URL--------------', $pURL),
+        array('-----------IFRAME URL------------', $iURL)
     );
 
 
     # Get plans
-    $plans_paginator = $mc2p->plan->itemList(null);
-    $count = $plans_paginator->count;
-    $results = $plans_paginator->results; # Application's plans
-    $nextList = $plans_paginator->getNextList();
+    $plansPaginator = $mc2p->plan->itemList(null);
+    $count = $plansPaginator->count;
+    $results = $plansPaginator->results; # Application's plans
+    $nextList = $plansPaginator->getNextList();
 
     var_dump(
-        array('------------PLAN----------------'),
-        array('------------COUNT---------------', $count),
-        array('------------RESULTS-------------', $results),
-        array('------------NEXT LIST-----------', $nextList)
+        array('----------PLAN TEST--------------'),
+        array('------------COUNT----------------', $count),
+        array('-----------RESULTS---------------', $results),
+        array('----------NEXT LIST--------------', $nextList)
     );
 
 
@@ -63,8 +63,8 @@ try {
     $product->save();
 
     var_dump(
-        array('------------PRODUCT----------------'),
-        array('------------ID---------------------', $product->id)
+        array('-------PRODUCT TEST--------------'),
+        array('------------ID-------------------', $product->id)
     );
 
 
@@ -79,10 +79,85 @@ try {
     $tax->delete();
 
     var_dump(
-        array('------------TAX----------------'),
-        array('------------ID---------------------', $tax->id)
+        array('---------TAX TEST----------------'),
+        array('------------ID-------------------', $tax->id)
     );
 
+
+    # Check if transaction was paid
+    $transaction = $mc2p->Transaction(
+        array(
+            "id" => "c8325bb3-c24e-4c0c-b0ff-14fe89bf9f1f"
+        )
+    );
+    $transaction->retrieve();
+
+    var_dump(
+        array('---TRANSACTION PAID TEST---------'),
+        array('------------PAID-----------------', $transaction->status == 'D')
+    );
+
+
+    # Create subscription
+    $subscription = $mc2p->Subscription(
+        array(
+            "currency" => "EUR",
+            "plan" => array(
+                "name" => "Plan",
+                "price" => 5,
+                "duration" => 1,
+                "unit" => "M",
+                "recurring" => True
+            ),
+            "note" => "Note example"
+        )
+    );
+    $subscription->save();
+    $pURL = $subscription->getPayUrl();
+    $iURL = $subscription->getIframeUrl();
+
+    var_dump(
+        array('--------SUBSCRIPTION TEST--------'),
+        array('------------PAY URL--------------', $pURL),
+        array('-----------IFRAME URL------------', $iURL)
+    );
+
+
+    # Receive a notification
+    $notificationData = $mc2p->NotificationData(array(
+        "status" => "D",
+        "type" => "P",
+        "id" => "c8325bb3-c24e-4c0c-b0ff-14fe89bf9f1f",
+        "sale_id" => "d1bb7082-7a97-48c6-893d-4d5febcd463b"
+    ), $mc2p);
+    $notificationStatus = $notificationData->getStatus(); # Paid
+    $notificationTransaction = $notificationData->getTransaction(); # Transaction Paid
+    $notificationSale = $notificationData->getSale(); # Sale generated
+
+    var_dump(
+        array('-------NOTIFICATION TEST---------'),
+        array('-----------STATUS----------------', $notificationStatus),
+        array('---------TRANSACTION-------------', $notificationTransaction->id),
+        array('------------SALE-----------------', $notificationSale->id)
+    );
+
+
+    # Incorrect data
+    $shipping = $mc2p->Shipping(
+        array(
+            "name" => "Normal shipping",
+            "price" => "text" # Price must be number
+        )
+    );
+
+    try {
+        $shipping->save();
+    } catch (MC2P\InvalidRequestMC2PError $e) {
+        var_dump(
+            array('----------ERROR TEST---------'),
+            array('---------STATUS CODE---------', $e->getMessage())
+        );
+    }
 
 } catch (Exception $e) {
     var_dump($e);
